ad2c2327cca50ad00e9ef697dd2857497e8f8c0c
JanzenLiu <chengtsung_liu@yahoo.com>
2019-01-20 11:21:14 +0800

    fix typo

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
+from setuptools import setup
 
 setup(name='unobot',
       version='0.0.1',
-      install_requires=['gym, gym_uno, baselines'], # And any other dependencies foo needs
+      install_requires=['gym', 'gym_uno', 'baselines'], # And any other dependencies foo needs
 )
